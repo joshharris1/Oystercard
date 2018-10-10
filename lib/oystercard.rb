69c6aea8c68f32ae3df8c93c013e84adc728cd72
Josh Harris <josh.harris212@yahoo.co.uk>
2018-10-10 13:51:35 +0000

    Updated - step 9, still not working

---
+++ lib/oystercard.rb
@@ -0,0 +1,35 @@
+class Oystercard
+
+   attr_reader :balance
+   #attr_reader :is_in_journey
+   MAXIMUM_BALANCE = 90
+
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(amount)
+    fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if amount + @balance > MAXIMUM_BALANCE
+    @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def in_journey?
+    @is_in_journey
+  end
+
+ def touch_in
+   fail "Insufficient balance to touch in" if @balance < 1
+   @is_in_journey = true
+ end
+
+ def touch_out
+   @is_in_journey = false
+ end
+
+
+
+end
---
+++ spec/oystercard_spec.rb
@@ -0,0 +1,53 @@
+require 'oystercard'
+describe Oystercard do
+
+   it 'has zero initial balance' do
+
+      expect(subject.balance).to eq(0)
+
+   end
+
+   it 'will not touch in if below minimum balance' do
+     subject.top_up(1)
+     expect{ subject.touch_in }.to raise_error "Insufficient balance to touch in"
+   end
+
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
+   it 'deducts an amount from the balance' do
+     subject.top_up(20)
+     expect{ subject.deduct 3}.to change{ subject.balance }.by (-3)
+   end
+
+   describe '#topup' do
+
+       it { is_expected.to respond_to(:top_up).with(1).argument }
+
+       it 'can top up balance' do
+           expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+       end
+
+       it 'raises an error if the maximum balance is exceeded' do
+         maximum_balance = Oystercard::MAXIMUM_BALANCE
+         subject.top_up maximum_balance
+         expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+     end
+
+   end
+
+   it 'is initially not in a journey' do
+     expect(subject).not_to be_in_journey
+   end
+
+   it "can touch in" do
+     subject.touch_in
+     expect(subject).to be_in_journey
+   end
+
+   it "can touch out" do
+     subject.touch_in
+     subject.touch_out
+     expect(subject).not_to be_in_journey
+   end
+
+end
